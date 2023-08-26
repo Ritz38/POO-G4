1be4d19e97c8d0c7d87666cb9f88289f8b5f4965
Juan Pablo Zuluaga Mesa <juanpablozuluagamesa@gmail.com>
2023-08-26 11:58:51 -0500

    Actividad2

---
+++ Actividad_2/Parte1/EjercicioPro18/src/EjercicioPropuesto18.java
@@ -0,0 +1,6 @@
+package Actividad_2.Parte1.EjercicioPro18.src;
+public class EjercicioPropuesto18 {
+    public static void main(String[] args){
+        System.out.println("Hello, World!");
+    }
+}
